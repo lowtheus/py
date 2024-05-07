007f86551c9f2fa8521fc29c7ea1c0a6b8f257c7
lowtheus <169102519+lowtheus@users.noreply.github.com>
2024-05-06 22:14:45 -0300

    Add files via upload

---
+++ aula12.js
@@ -0,0 +1,63 @@
+//Em JavaScript, uma string é um texto de dados armazenado. 
+//Pode ser definida de múltiplas formas dentro de aspas simples(''),
+//aspas duplas(""), ou backticks(``)
+
+//strings são  sequências (ou cadeias) de caracteres que usamos para,
+// entre outras coisas, manipular textos.
+
+//strings são sequências de caracteres alfanuméricos 
+//(letras, números e/ou símbolos)
+
+tx1 = '123456789qwerty@#$%¨&*!()❤🎂🎀💎';
+tx2 = "123456789qwerty@#$%¨&*!()❤🎂🎀💎";
+tx3 = `123456789qwerty@#$%¨&*!()❤🎂🎀💎`;
+
+//document.write(tx1);
+//document.write(tx2);
+//document.write(tx3);
+// Em Javascript, uma string sempre estará entre aspas, aspas duplas 
+//ou backticks
+
+//Podemos colocar nossas strings entre aspas duplas ou simples.
+//Para o JavaScript, não há diferença, já que ele considera as 
+//duas formas de declaração válidas. Mas, atenção, 
+//essa regra pode não se aplicar a outras linguagens
+//-------------------==========================------------------------------
+//Em alguns momentos, a string poderá ser um texto que contém aspas. 
+//Nesses casos, é preciso combinar a utilização das aspas simples 
+//com aspas duplas e vice-versa, porque um texto como
+
+//var mensagem = 'Ela disse 'Adeus'';
+//ou
+//var mensagem1 = "Ela disse "Adeus"";
+// não funciona corretamente
+//var mensagem2 = 'Ela disse"Adeus"';
+//ou
+//var mensagem3 = "Ela disse 'Adeus'";
+//funciona corretamente.
+
+//document.write(mensagem);
+//document.write(mensagem1);
+//document.write(mensagem2);
+//document.write(mensagem3);
+
+
+//-------------------==========================------------------------------
+
+//começar uma string com um tipo de aspas e terminar com outro também 
+//causa erro
+
+//var mensagem4 = 'Um, dois, tres...";
+//document.write(mensagem4);
+
+
+//-------------------==========================------------------------------
+
+//textos em inglês (principalmente) que tem palavras com
+// aspas podem apresentar problemas
+
+//var mensagem5 = 'Let's just say I'm a ghost';
+//document.write(mensagem5);
+
+//-------------------==========================------------------------------
+
